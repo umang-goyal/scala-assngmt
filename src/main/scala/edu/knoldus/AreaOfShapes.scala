9b33a823fc0a77f932ba3ab58761dc12a7bcc5d7
umang-goyal <goel97umang@gmail.com>
2020-02-02 00:39:31 +0530

    fixed a small bug, misinterpreted question of finding area of shape

--- src/main/scala/edu/knoldus/AreaOfShapes.scala
+++ src/main/scala/edu/knoldus/AreaOfShapes.scala
@@ -2,15 +2,13 @@ package edu.knoldus
 
 class AreaOfShapes {
 
-  val function = (firstDimension: Int, secondDimension: Int)=> firstDimension*secondDimension
+  def calArea(firstDimension: Int, secondDimension: Int, f:(Int,Int) => Int){ f(firstDimension,secondDimension)}
 
-  def area(shape: String, firstDimension: Int, secondDimension: Int, f:(Int,Int) => Int):String={
+  def area(shape: String, firstDimension: Int, secondDimension: Int):String=
     shape match {
-      case "rectangle" => s"area of $shape is ${f(firstDimension, secondDimension)}"
-      case "parallelogram" => s"area of $shape is ${f(firstDimension, secondDimension)}"
-      case "rhombus" => s"area of $shape is ${f(firstDimension, secondDimension)}"
-      case _ => s"not defined yet for $shape"
-    }
+    case "rectangle" => s"area of $shape is ${calArea(firstDimension, secondDimension, (a,b)=> a*b )}"
+    case "parallelogram" => s"area of $shape is ${calArea(firstDimension, secondDimension, (a,b)=> a*b) }"
+    case "rhombus" => s"area of $shape is ${calArea(firstDimension, secondDimension, (a,b)=> (a*b)/2 )}"
+    case _ => s"not defined yet for $shape"
   }
 }
-
--- src/test/scala/edu/knoldus/AreaOfShapesTest.scala
+++ src/test/scala/edu/knoldus/AreaOfShapesTest.scala
@@ -7,19 +7,19 @@ class AreaOfShapesTest extends FunSuite {
   val shape = new AreaOfShapes
 
   test("area when shape is rectangle") {
-    shape.area ("rectangle", 5, 2, shape.function)
+    shape.area ("rectangle", 5, 2)
   }
 
   test("area when shape is parallelogram") {
-    shape.area ("parallelogram", 7, 2, shape.function)
+    shape.area ("parallelogram", 7, 2)
   }
 
   test("area when shape if rhombus") {
-    shape.area ("rhombus", 8, 2, shape.function)
+    shape.area ("rhombus", 8, 2)
   }
   
   test("area when shape if square") {
-    shape.area ("square", 9, 2, shape.function)
+    shape.area ("square", 9, 2)
   }
 
 }
